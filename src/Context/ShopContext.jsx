cbbb6ce39d01d64547e005fe80c9ad95c2c62ffb
Dimple Singh <dimplesingh@Dimples-MacBook-Air.local>
2025-02-21 00:00:07 -0600

    Products and Cart pages added

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,17 +1,57 @@
-import React, { createContext } from "react";
+import React, { useState, createContext } from "react";
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length + 1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
 
-    const contextValue = { all_product};
+    const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    return(
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        console.log(cartItems);
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount=0;
+        for (const key in cartItems) {
+            if(cartItems[key]>0){
+                let itemInfo = all_product.find((product) => product.id === Number(key));
+            totalAmount += itemInfo.new_price * cartItems[key];
+            }
+            
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItems = () => {
+        let totalItems = 0;
+        for (const key in cartItems) {
+            if(cartItems[key]>0){
+            totalItems += cartItems[key];
+            }
+        }
+        return totalItems;
+    }
+    const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+    return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
-            </ShopContext.Provider>
-        )
-    }
+        </ShopContext.Provider>
+    )
+}
 
 export default ShopContextProvider;
